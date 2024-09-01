28b4105074f9ba2a544d53e53c1cbf8836260111
HANYONUJUN <gksdydwns34@naver.com>
2024-09-01 22:04:08 +0900

    dynamic type이란

---
+++ bin/dart_test_3.dart
@@ -0,0 +1,14 @@
+// Dynamic
+// 여러가지 타입을 가질 수 있는 변수에 사용 가능한 키워드
+// 해당 변수의 type을 알 수 없을 때 주로 사용
+// 변수 선언 시 dynamic을 쓰거나 값을 지정하지 않으면 dynamic type을 가짐
+void main() {
+  dynamic name;
+  if(name is String) {
+    
+  }
+
+  if(name is int) {
+  
+  }
+}
